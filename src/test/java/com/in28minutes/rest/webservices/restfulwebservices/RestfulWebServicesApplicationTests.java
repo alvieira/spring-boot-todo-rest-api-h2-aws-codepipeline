b9e09700f9f8820dd31977efeb4f268c4e437422
Aloisio Vieira <aloisio.vieira@gmail.com>
2021-02-28 19:50:17 -0300

    first commit

---
+++ src/test/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
@@ -0,0 +1,12 @@
+package com.in28minutes.rest.webservices.restfulwebservices;
+
+import org.junit.jupiter.api.Test;
+
+class RestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+	}
+
+}
